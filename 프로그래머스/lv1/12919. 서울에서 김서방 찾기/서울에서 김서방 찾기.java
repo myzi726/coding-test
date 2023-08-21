eb6524b0c2550bb4e3f354aa67d492068236fdad
명지 <83940700+myzi726@users.noreply.github.com>
2023-08-21 20:14:42 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.11 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,11 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "";
+        int x = 0;
         
-        int n = Arrays.asList(seoul).indexOf("Kim");
+        for(int i=0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                x = i;
+            }
+        }
         
-        answer = "김서방은 " + n +"에 있다";
+        String answer = "김서방은 " + x + "에 있다";
         
         return answer;
     }
